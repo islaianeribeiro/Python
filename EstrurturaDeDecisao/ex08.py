eceb6b7aa33ddd830e08a6a3a3ad14419abf7986
Islaiane Ribeiro <islaiane.lribeiro@gmail.com>
2024-07-12 16:01:21 -0300

    update ex08

--- EstrurturaDeDecisao/ex08.py
+++ EstrurturaDeDecisao/ex08.py
@@ -1,18 +1,41 @@
 # Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 
-# Ainda resolvendo.
 pro1 = float(input("Digite o valor do primeiro produto: "))
 pro2 = float(input("Digite o valor do segundo produto: "))
 pro3 = float(input("Digite o valor do terceiro produto: "))
 
-if pro1 <= pro2 and pro1 <= pro3:
-    menor = pro1
-    p = "primeiro"
-elif pro2 <= pro1 and pro2 <= pro3:
-    menor = pro2
-    p = "segundo"
-else:
-    menor = pro3
-    p = "terceiro"
+def menor():
+#Se houver apenas um número menor.
+    if pro1 < pro2 and pro1 < pro3:
+        print('--------------------------------------')
+        print(f'O produto 1 é o mais barato.')
+        print(f'Seu valor é de R${pro1}')
+    elif pro2 < pro1 and pro2 < pro3:
+        print('--------------------------------------')
+        print(f'O produto 2 é o mais barato.')
+        print(f'Seu valor é de R${pro2}')
+    elif pro3 < pro1 and pro3 < pro2:
+        print('--------------------------------------')
+        print(f'O produto 3 é o mais barato.')
+        print(f'Seu valor é de R${pro3}')
+#Se alguns números forem iguais.
+    elif pro1 == pro2 and pro1 < pro3:
+        print('--------------------------------------')
+        print(f'Os produtos 1 e 2 são os mais baratos.')
+        print(f'Seu valor é de R${pro1}')
+    elif pro1 == pro3 and pro1 < pro2:
+        print('--------------------------------------')
+        print(f'Os produtos 1 e 3 são os mais baratos.')
+        print(f'Seu valor é de R${pro1}')
+    elif pro2 == pro3 and pro2 < pro1:
+        print('--------------------------------------')
+        print(f'Os produtos 2 e 3 são os mais baratos.')
+        print(f'Seu valor é de R${pro2}')
+#Se todos os números forem iguais.
+    else:
+        print('--------------------------------------')
+        print(f'Todos os preços são iguais, então não há menor valor.')
+        print(f'O valor é de R${pro1}')
 
-print(f"Você deve comprar o {p} produto, pois seu valor é R${menor}, o mais barato dos produtos.")
+
+menor()
